352ab9d7747e7bba53b106a539fe17d1cfb6f660
aishcann <aish.cann@gmail.com>
2021-05-19 21:44:29 -0400

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>The Future is Clearer</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/ThreeCardPull.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ThreeCardPull = () => {
+    return (
+        <div>
+           <section>card one</section> 
+           <section>card two</section>
+           <section>card three</section>
+        </div>
+    );
+};
+
+export default ThreeCardPull;
